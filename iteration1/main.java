92e5142abd6662dcb2de9cbe905a8cf305b0152b
Ömer Deligöz <79800277+omerdeligoz@users.noreply.github.com>
2023-11-15 17:04:18 +0300

    Add files via upload

--- iteration1/main.java
+++ iteration1/main.java
@@ -1,1 +1,11 @@
-i
+package iteration1;
+
+import java.io.IOException;
+
+public class main {
+    public static void main(String[] args) throws IOException {
+        CourseRegistrationSystem courseRegistrationSystem = new CourseRegistrationSystem();
+        courseRegistrationSystem.start();
+
+    }
+}
