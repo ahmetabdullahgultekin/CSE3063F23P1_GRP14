91badb293fc6340ed6e124ba760fb1f213a36d4e
omerdeligoz <deligozomer@hotmail.com>
2023-11-15 17:43:27 +0300

    Add functionality to mainMenu in CourseRegistrationSystem
    Implemented functionality such as user input and menu choices to the mainMenu method in the CourseRegistrationSystem class. This allows users to navigate through the program and makes the application more interactive and user-friendly.

--- iteration1/CourseRegistrationSystem.java
+++ iteration1/CourseRegistrationSystem.java
@@ -11,7 +11,26 @@ public class CourseRegistrationSystem implements IDisplayMenu {
     }
 
     public void mainMenu(Department department) {
-      
+        printMenu("mainMenu");
+
+        choice = getInput();
+        if (choice == -1) {
+            mainMenu(department);
+        } else {
+            switch (choice) {
+                case 0:
+                    System.out.println("Exiting from system");
+                    exitProgram();
+                    break;
+                case 1:
+                    loginMenu(department);
+                    break;
+                default:
+                    System.out.println("Invalid choice Please select again");
+                    mainMenu(department);
+                    break;
+            }
+        }
     }
 
     public void loginMenu(Department department) {
