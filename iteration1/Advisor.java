8c61b66f78a3ff53e9d5edf93bd52b98c60c8457
omerdeligoz <deligozomer@hotmail.com>
2023-11-17 00:10:45 +0300

    "Made fields private and improved input validation"

--- iteration1/Advisor.java
+++ iteration1/Advisor.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
  */
 public class Advisor extends Lecturer implements IDisplayMenu {
 
-    private List<iteration1.Student> studentsAdvised;
+    private List<Student> studentsAdvised;
     private List<Registration> requestList;
     private int requestNumber;
 
@@ -51,11 +51,18 @@ public class Advisor extends Lecturer implements IDisplayMenu {
 
         System.out.print("Enter the request number you want to evaluate: ");
         Scanner scanner = new Scanner(System.in);
-        requestNumber = scanner.nextInt();
+
+        try {
+            requestNumber = scanner.nextInt();
+        } catch (Exception e) {
+            System.out.println("Invalid input, please enter a number");
+            printRequests();
+        }
+
 
         if (requestNumber == 0) {
             return;
-        } else if (requestNumber > requestList.size()) {
+        } else if (requestNumber > requestList.size() || requestNumber < 0) {
             System.out.println("Invalid choice Please select again");
             printRequests();
         } else {
@@ -77,7 +84,14 @@ public class Advisor extends Lecturer implements IDisplayMenu {
         System.out.println("3. Back");
         System.out.println("Please select an option: ");
         Scanner scanner = new Scanner(System.in);
-        int choice = scanner.nextInt();
+
+        int choice = 0;
+        try {
+            choice = scanner.nextInt();
+        } catch (Exception e) {
+            System.out.println("Invalid input, please enter a number");
+            replyRequests();
+        }
 
         switch (choice) {
             case 1:
@@ -122,7 +136,7 @@ public class Advisor extends Lecturer implements IDisplayMenu {
         return this.getUserName().equals(userName) && this.getPassword().equals(password);
     }
 
-    public List<iteration1.Student> getStudentsAdvised() {
+    public List<Student> getStudentsAdvised() {
         return studentsAdvised;
     }
 
