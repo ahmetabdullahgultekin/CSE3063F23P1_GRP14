0741cdc3de2fad7fdda9cf83d222b97985d10e9c
omerdeligoz <deligozomer@hotmail.com>
2023-11-16 16:27:21 +0300

    Add new JSONWriter class for data serialization
    Introduced a new class, JSONWriter, which aims to handle the serialization of different types of data objects such as Departments and Students into JSON format. This Java class includes initial method signatures for functionalities that are planned to be released in the later stages, such as writing transcripts, requests, and student data. The introduction of a dedicated JSONWriter class will centralize all JSON serialization logic, making the codebase more manageable and improve readability.

---
+++ iteration1/JSONWriter.java
@@ -0,0 +1,43 @@
+package iteration1;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import java.io.File;
+import java.io.IOException;
+
+public class JSONWriter {
+
+    Department department;
+
+    ObjectMapper objectMapper;
+
+    JsonNode jsonNode;
+
+
+    public void start(Department department) {
+
+    }
+
+
+    public void writeJson() {
+
+    }
+
+
+    public void writeTranscripts() {
+
+    }
+
+    public void writeRequests() {
+
+    }
+
+
+    public void writeStudents() {
+    }
+}
+
