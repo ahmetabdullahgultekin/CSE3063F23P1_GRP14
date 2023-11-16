b5604812e5c16cd05d603270ae6a4450dca6ada4
hamzalis <hamza613468@gmail.com>
2023-11-16 18:28:07 +0300

    Update Student.java
    Some changes have been made:
    -2 attributes were removed.
    -The sendRequest method has been implemented.
    -The printMenu has been implemented.
    -Get and set methods have been rearranged.

--- iteration1/Student.java
+++ iteration1/Student.java
@@ -1,6 +1,9 @@
 package iteration1;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Student extends Person implements IDisplayMenu {
 
@@ -9,36 +12,60 @@ public class Student extends Person implements IDisplayMenu {
     private Transcript transcript;
     private byte gradeLevel;
     private boolean hasRequest;
-    private List<Course> selectedCourses;
     private List<Course> draft;
     private List<Course> availableCourses;
-    private Map<Course, CourseSection> courseSections;
 
     //Implement Constructor
     public Student(int studentID, String name, String surname, String userName, String password, byte gradeLevel) {
         super(studentID, name, surname, userName, password);
-        this.selectedCourses = new ArrayList<>();
         this.draft = new ArrayList<>();
         this.availableCourses = new ArrayList<>();
-        this.courseSections = new HashMap<>();
         this.gradeLevel = gradeLevel;
     }
 
+    //Check if there is already a request awaiting approval
+    //Check if the draft is empty
+    /* Create a new Registration object and initialize it with the current
+    student object (this) and draft information */
     public void sendRequest() {
-        // Send request logic
+        if (hasRequest) {
+            System.out.println("\nYou already have a request waiting for approval");
+        } else if (draft.isEmpty()) {
+            System.out.println("\nYour draft is empty!");
+        } else {
+            Registration registration = new Registration(this, draft);
+            registration.addRequest(advisor);
+        }
     }
 
     //Implements the printMenu method from the IDisplayMenu interface.
     //Prints a menu based on the specified menu type.
+    /* After determining whether the user is a student or an advisor
+    based on the String value, print the relevant menu. */
     @Override
     public void printMenu(String menuType) {
         switch (menuType) {
             case "studentMenu":
-                // Student menu printing logic
+                System.out.println("\nStudent Menu");
+                System.out.println("Welcome " + this.getName() + " " + this.getSurname() + "!");
+                System.out.println("Please select from the following options:");
+                System.out.println("0. Exit");
+                System.out.println("1. Course Selection Menu");
+                System.out.println("2. View Transcript");
+                System.out.println("3. Log out");
+                System.out.print("Enter your choice: ");
                 break;
             case "courseSelectionMenu":
-                // Course selection menu printing logic
-                break;
+                System.out.println("\nCourse Selection Menu");
+                System.out.println("Please select from the following options:");
+                System.out.println("0. Back");
+                System.out.println("1. Course Status Check");
+                System.out.println("2. Add Course");
+                System.out.println("3. Drop Course");
+                System.out.println("4. Send Request");
+                System.out.println("5. Show request status");
+                System.out.println("6. Log out");
+                System.out.print("Enter your choice: ");
         }
     }
 
@@ -54,8 +81,8 @@ public class Student extends Person implements IDisplayMenu {
     //and prints an appropriate message to the console.
     public void showRequestStatus() {
         if (hasRequest) {
-            System.out.println("Your request is waiting for advisor approval");
-        } else System.out.println("There is no waiting request");
+            System.out.println("\nYour request is waiting for advisor approval");
+        } else System.out.println("\nThere is no waiting request");
     }
 
     public List<Course> getAvailableCourses() {
@@ -100,24 +127,12 @@ public class Student extends Person implements IDisplayMenu {
         return gradeLevel;
     }
 
-    public boolean isHasRequest() {
-        return hasRequest;
-    }
-
     public void setHasRequest(boolean hasRequest) {
         this.hasRequest = hasRequest;
     }
 
-    public List<Course> getSelectedCourses() {
-        return selectedCourses;
-    }
-
     public List<Course> getDraft() {
         return draft;
-    }
-
-    public Map<Course, CourseSection> getCourseSections() {
-        return courseSections;
     }
 
     public Transcript getTranscript() {
@@ -126,5 +141,9 @@ public class Student extends Person implements IDisplayMenu {
 
     public void setTranscript(Transcript transcript) {
         this.transcript = transcript;
+    }
+
+    public void setDraft(List<Course> draft) {
+        this.draft = draft;
     }
 }
