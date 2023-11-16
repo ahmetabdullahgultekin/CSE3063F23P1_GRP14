98f9cb6699b391e6616196aec79955e21274cfdb
hamzalis <hamza613468@gmail.com>
2023-11-16 23:09:34 +0300

    Final version of Student.java
    All of the cases are completed

--- iteration1/Student.java
+++ iteration1/Student.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
+//Inherits from the Person class and implements the ILogin interface
 public class Student extends Person implements IDisplayMenu {
 
     //All attributes
@@ -69,12 +70,110 @@ public class Student extends Person implements IDisplayMenu {
         }
     }
 
+    // The 'addCourse' function displays all accessible courses and adds these courses to
+    // the student's draft, which is a private attribute.
     public void addCourse() {
-        // Add course logic
+        //If a request has been sent, terminate the function.
+        if (hasRequest) {
+            System.out.println("\nYou can not add lecture because you have a request waiting for approval.");
+            return;
+        }
+        //Call the calculateNumberOfCourses() function to limit with the maximum number of courses.
+        int numberOfCourses = calculateNumberOfCourses();
+        if (numberOfCourses >= this.getDepartment().getMaxCourseNumber()) {
+            System.out.println("You can not add more lectures.");
+            return;
+        }
+
+        //Call the viewAvailableCourses() function to see all available courses.
+        System.out.println("\nAdd Course Menu");
+        viewAvailableCourses();
+
+        System.out.println("Here is the available courses:");
+        System.out.println("0. Back");
+        //Yazdırma işlemi için belli bir for döngüsü kullan
+        for (int i = 0; i < this.getAvailableCourses().size(); i++) {
+            System.out.println((i + 1) + ". " + this.getAvailableCourses().get(i).getCourseCode() +
+                    " - " + this.getAvailableCourses().get(i).getCourseName());
+        }
+        //Check the controlGate variable for while loop manipulation.
+        int controlGate = 1;
+        while (controlGate == 1) {
+            //Take input from the user and make assignments to the courses.
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Choose number between 1 to " + this.getAvailableCourses().size() + " to add course: ");
+            int userNumberInput = 0;
+            try{
+                userNumberInput = scanner.nextInt();
+            }
+            catch (Exception e){
+                System.out.println("Invalid input, please enter a number");
+                addCourse();
+            }
+            //Back to Course Section Menu with return value
+            if (userNumberInput == 0)
+                return;
+            //If incorrect input is entered, ask for input again.
+            if (1 <= userNumberInput && userNumberInput <= this.getAvailableCourses().size()) {
+                controlGate = 0;
+                this.draft.add(this.getAvailableCourses().get(userNumberInput - 1));
+                this.getAvailableCourses().remove(userNumberInput - 1);
+                addCourse();
+            } else {
+                System.out.println("Invalid input, please enter a valid number");
+            }
+        }
+    }
+
+    // This method calculates the total number of courses, considering both the drafted courses
+    // and the courses in the transcript for which the student has not received a grade yet.
+    private int calculateNumberOfCourses() {
+        int numberOfCourses = draft.size();
+        for (Course course : this.getTranscript().getCourseGradeMap().keySet()) {
+            if (this.getTranscript().getCourseGradeMap().get(course) == null) {
+                numberOfCourses++;
+            }
+        }
+        return numberOfCourses;
     }
 
     public void dropCourse() {
-        // Drop course logic
+        // Check if the student has already sent a request
+        if (hasRequest) {
+            System.out.println("\nYou can not drop lecture because you have a request waiting for approval.");
+            return;
+        }
+
+        System.out.println("\nDrop Course Menu");
+        System.out.println("Select the course you want drop");
+
+        // Check if the draft is empty.
+        if (draft.isEmpty()) {
+            System.out.println("Your draft is empty!");
+        } else {
+            System.out.println("0. Back");
+            // Manipulation for displaying and dropping courses in the menu.
+            for (int i = 0; i < draft.size(); i++) {
+                System.out.println((i + 1) + ". " + draft.get(i).getCourseCode() +
+                        " - " + draft.get(i).getCourseName());
+            }
+            // Remove input index element -1 from the draft list if the input is valid and in the appropriate range.
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Choose number between 1 to " + this.draft.size() + " to drop course: ");
+            int userNumberInput = 0;
+            try{
+                userNumberInput = scanner.nextInt();
+            }
+            catch (Exception e){
+                System.out.println("Invalid input, please enter a number");
+                dropCourse();
+            }
+
+            if (userNumberInput == 0)
+                return;
+            draft.remove(userNumberInput - 1);
+            dropCourse();
+        }
     }
 
     //Checks if the student has a registration request waiting for advisor approval
@@ -83,14 +182,61 @@ public class Student extends Person implements IDisplayMenu {
         if (hasRequest) {
             System.out.println("\nYour request is waiting for advisor approval");
         } else System.out.println("\nThere is no waiting request");
+        System.out.println("Your draft: ");
+        for (Course course : draft){
+            System.out.println(course.getCourseCode() + "-" + course.getCourseName());
+        }
+
     }
 
     public List<Course> getAvailableCourses() {
         return availableCourses;
     }
 
-    public ArrayList<Course> viewAvailableCourses() {
-        //Create a function that captures the logic for viewing available courses based on specific conditions.
+    public void viewAvailableCourses() {
+        /*
+        Collect all course sections from the department, and for each course, store them in
+        a map<course,grade> along with the grade value. Also, keep track of course sections
+        with their respective courses.
+        */
+        ArrayList<Course> availableCourses = new ArrayList<>();
+        List<CourseSection> allCourseSections = this.getDepartment().getCourseSections();
+        Map<Course, Grade> mapGrade = this.getTranscript().getCourseGradeMap();
+        Map<CourseSection, Course> courseSectionCourse = this.getDepartment().getSectionCourseMap();
+
+        /*
+        "While iterating through all course sections, check if the grade level is sufficient
+         and if the course is already in the draft. If it is, continue the loop to change the course section.
+         */
+        for (CourseSection courseSection : allCourseSections) {
+            if (this.gradeLevel < courseSection.getGradeLevel() || draft.contains(courseSectionCourse.get(courseSection)))
+                continue;
+        /*
+        If the conditions are not problematic and there is no course in the transcript,
+        enter the 'if' statement. If status is true, then the student can add.
+        If status is false, the student is ineligible unless they have taken the course before
+        and received a low success grade; in this case, they can retake the course.
+        */
+            Course course = courseSectionCourse.get(courseSection);
+            if (!mapGrade.containsKey(course)) {
+                boolean status = true;
+                for (Course prerequisite : course.getPreRequisiteCourses()) {
+                    if ((mapGrade.get(prerequisite) == null || mapGrade.get(prerequisite).getLetterGrade().equals("FF") ||
+                            mapGrade.get(prerequisite).getLetterGrade().equals("FD"))) {
+                        status = false;
+                    }
+                }
+                if (status)
+                    availableCourses.add(course);
+            } else {
+                // If the student has previously taken the course and has a grade lower than 'CC', include it
+                if (mapGrade.get(course) != null && mapGrade.get(course).getLetterGrade().compareTo("CC") > 0) {
+                    availableCourses.add(course);
+                }
+            }
+        }
+
+        this.availableCourses = availableCourses;
     }
 
     //Checks if the provided username and password match the student's credentials.
