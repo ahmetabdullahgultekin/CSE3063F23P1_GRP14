894fe7cc83cf15b30cf87b286724906e3dd5a312
omerdeligoz <deligozomer@hotmail.com>
2023-11-16 17:56:26 +0300

    "Update JSONWriter to write student transcripts to JSON files
    Added functionality in JSONWriter.java to iterate over each student in the department and write their transcript information to individual JSON files. This update helps in persisting updated transcript data for each student."

--- iteration1/JSONWriter.java
+++ iteration1/JSONWriter.java
@@ -32,7 +32,47 @@ public class JSONWriter {
 
 
     public void writeTranscripts() {
+        String filePath;
 
+        for (Student student : department.getStudents()) {
+            filePath = "iteration1/jsons/Transcripts/" + student.getID() + ".json";
+            try {
+                objectMapper = new ObjectMapper();
+                jsonNode = objectMapper.readTree(new File(filePath));
+
+                int newTakenCredits = student.getTranscript().getTakenCredits();
+                int newCompletedCredits = student.getTranscript().getCompletedCredits();
+                double newCgpa = student.getTranscript().getCgpa();
+                newCgpa = Math.round(newCgpa * 100.0) / 100.0;
+
+                ((ObjectNode) jsonNode).put("takenCredits", newTakenCredits);
+                ((ObjectNode) jsonNode).put("completedCredits", newCompletedCredits);
+                ((ObjectNode) jsonNode).put("cgpa", newCgpa);
+
+                ArrayNode newCoursesArray = JsonNodeFactory.instance.arrayNode();
+
+                for (Course course : student.getTranscript().getStudentCourses()) {
+                    ObjectNode courseNode = JsonNodeFactory.instance.objectNode();
+                    courseNode.put("courseCode", course.getCourseCode());
+                    if (student.getTranscript().getCourseGradeMap().get(course) == null) {
+                        courseNode.put("letterGrade", (JsonNode) null);
+                    } else {
+                        courseNode.put("letterGrade", student.getTranscript().getCourseGradeMap().get(course).getLetterGrade());
+                    }
+                    newCoursesArray.add(courseNode);
+                }
+
+                // Replace the existing "courses" array with the new array
+                ((ObjectNode) jsonNode).set("courses", newCoursesArray);
+
+                // Write the updated JsonNode back to the file
+                objectMapper.writeValue(new File(filePath), jsonNode);
+
+            } catch (IOException e) {
+                System.out.println("File not found");
+                System.exit(0);
+            }
+        }
     }
 
     public void writeRequests() {
