99612ba8af42f5c10ca66ad31f12a2efc7b68a01
MehrinKaya <103828700+MehrinKaya@users.noreply.github.com>
2023-12-14 21:21:31 +0300

    Update LaboratorySection.java

--- iteration2/LaboratorySection.java
+++ iteration2/LaboratorySection.java
@@ -2,33 +2,35 @@ package iteration2;
 
 /**
  * The LaboratorySection class represents a course with a laboratory offered by an educational institution in different departments.
- * It contains extra information about the laboratorySectionCode, capacity, assistant and numberOfStudents of the section.
+ * It contains extra information about the laboratorySectionCode, capacity, assistant and numberOfStudents,hour,day of the section.
  */
 public class LaboratorySection {
     private String laboratorySectionCode;
     private int capacity;
     private Assistant assistant;
     private int numberOfStudents;
+    private int hour;
+    private String day;
 
 
     /**
      * Creates a new LaboratorySection object with the given parameters
      * @param laboratorySectionCode      the section's code
      * @param capacity                   the section's capacity
-     * @param assistant                  the section's assistant
+     * @param hour                       the section's hour
+     * @param day                        the section's day
      */
 
-    public LaboratorySection(String laboratorySectionCode, Assistant assistant, int capacity) {
+   public LaboratorySection(String laboratorySectionCode, int capacity, int hour, String day) {
         this.laboratorySectionCode = laboratorySectionCode;
         this.capacity = capacity;
-        this.assistant = assistant;
-
+        this.hour = hour;
+        this.day = day;
     }
 
     public String getLaboratorySectionCode() {
         return laboratorySectionCode;
     }
-
     public int getNumberOfStudents() {
         return numberOfStudents;
     }
@@ -36,4 +38,9 @@ public class LaboratorySection {
     public void setNumberOfStudents(int numberOfStudents) {
         this.numberOfStudents = numberOfStudents;
     }
+    public void setAssistant(Assistant assistant) {
+        this.assistant = assistant;
+    }
+
+  
 }
