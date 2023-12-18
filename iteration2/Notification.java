739ed83415c4ca878271ab59faf10543f0ca32eb
hamzalis <hamza613468@gmail.com>
2023-12-18 23:31:40 +0300

    Update Notification.java
    Logger is added

--- iteration2/Notification.java
+++ iteration2/Notification.java
@@ -1,23 +1,24 @@
 package iteration2;
 
-import java.util.ArrayList;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Notification {
+    private static final Logger logger = LogManager.getLogger(Notification.class);
     private String message;
     private Person recipient;
 
 
-
-    public Notification(Person person, String message){
+    public Notification(Person person, String message) {
         this.recipient = person;
         this.message = message;
     }
 
-    public void sendNotification(){
-        if(recipient instanceof Advisor){
+    public void sendNotification() {
+        if (recipient instanceof Advisor) {
             ((Advisor) recipient).setNotification(this);
-        }
-        else if(recipient instanceof Student){
+        } else if (recipient instanceof Student) {
+            logger.info("Notification sent to student " + recipient.getID() + " that their request has been rejected.");
             ((Student) recipient).setNotification(this);
         }
 
