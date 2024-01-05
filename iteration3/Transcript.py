aa24052967d603c3585eafffc0982ad7d5a13906
omerdeligoz <deligozomer@hotmail.com>
2024-01-05 11:00:42 +0300

    Some methods converted to python

--- iteration3/Transcript.py
+++ iteration3/Transcript.py
@@ -16,3 +16,28 @@ class Transcript:
         self.__takenCredits = self.calculateTakenCredits()
         self.__completedCredits = self.calculateCompletedCredits()
 
+    def calculateCgpa(self):
+        if not self.__studentCourses:
+            return 0
+        totalGrade = 0
+        totalCredits = 0
+        for course in self.__studentCourses:
+            grades = self.__courseGradeMap.get(course, [])
+            if grades and len(grades) > 0:
+                grade = grades[-1].getGradeOver4()
+                credit = course.getCourseCredit()
+                if grade is not None and credit is not None:
+                    totalGrade += grade * credit
+                    totalCredits += credit
+
+        if totalCredits == 0:
+            return 0
+
+        self.__cgpa = totalGrade / totalCredits
+        return self.__cgpa
+
+    def calculateTakenCredits(self):
+        totalCredits = sum(course.getCourseCredit() for course in self.__studentCourses)
+        self.__takenCredits = totalCredits
+        return totalCredits
+
